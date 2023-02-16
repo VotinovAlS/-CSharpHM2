5935f43084a642ca56dae01bef561b017d3cae22
Nick <nick00712@yandex.ru>
2023-02-17 01:50:02 +0500

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите трехзначное число число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+int second = (number % 100 - number % 10) / 10;
+
+Console.WriteLine("Вторая цифра введенногочисла: " + second);
+}
+else
+{
+Console.WriteLine("Число не трехзначное!");
+}
