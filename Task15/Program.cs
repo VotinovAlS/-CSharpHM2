dbecadf0576bf37e3d0f71c4d6082e3deb8616a2
Nick <nick00712@yandex.ru>
2023-02-19 23:24:15 +0500

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 8)
+{
+  if (number == 6 || number == 7)
+  {
+    Console.WriteLine("День недели № " + number + " является выходным!");
+  }
+  else
+  {
+    Console.WriteLine("День недели № " + number + " НЕ является выходным!");
+  }
+}
+else
+{
+  Console.WriteLine("Такого дня недели НЕ существует!");
+}
+
