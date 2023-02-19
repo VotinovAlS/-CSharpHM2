b8edfd2833d714370cf5ea690c7ea7c3de0dd48f
Nick <nick00712@yandex.ru>
2023-02-19 23:13:52 +0500

    Task13!

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿Console.Write("Введите семизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int number = new Random().Next(1000000, 10000000);
+// Console.WriteLine("Сгенерированно число: " + number);
+
+if (number > 999999 && number < 10000000 || number > -10000000 && number < -999999)
+{
+  int thirdLeft = (number % 100000 - number % 10000) / 10000;
+  int thirdRight = (number % 1000 - number % 100) / 100;
+  if (thirdLeft >= 0 && thirdRight >= 0)
+  {
+    Console.WriteLine("третья цифра введенного числа слева: " + thirdLeft + "; " + "третья цифра введенного числа справа: " + thirdRight);
+  }
+  else
+  {
+    Console.WriteLine("третья цифра введенного числа слева: " + -thirdLeft + "; " + "третья цифра введенного числа справа: " + -thirdRight);
+  }
+}
+else
+{
+  if (number > 100 || number < -100)
+  {
+    Console.WriteLine("Число не семизначное!");
+  }
+  else
+  {
+    Console.WriteLine("Третьей цифры нет!");
+  }
+}
